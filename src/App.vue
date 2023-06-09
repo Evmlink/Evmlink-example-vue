6faff7d4886348bf8231abbfa1f6767182543601
Bulandy <18844116763@163.com>
2023-06-09 05:37:33 +0000

    init the base functions

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ConnectWallte/>
+  <Home />
+</template>
+<script>
+import ConnectWallte from './components/ConnectWallteExample.vue';
+import Home from "./Home.vue"
+export default {
+    components: {
+        ConnectWallte,
+        Home
+  },
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
